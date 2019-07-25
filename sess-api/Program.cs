1d05d06f2d77b33bbbeb2aaadf47e79858331786
Felipe Quintella <felipe@quintella.com>
2019-07-25 19:56:56 -0300

    RELEASE: 1.0.3

--- sess-api/Program.cs
+++ sess-api/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -19,18 +20,36 @@ namespace sess_api
                 .AddCommandLine(args)
                 .Build();
             
-            var hostUrl = configuration["hosturl"];
+            /*var hostUrl = configuration["hosturl"];
             if (string.IsNullOrEmpty(hostUrl))
-                hostUrl = "http://0.0.0.0:5000";
+                hostUrl = "http://0.0.0.0:5000";*/
             
-            CreateWebHostBuilder(args)
-                .UseUrls(hostUrl)
+            CreateWebHostBuilder(args, configuration)
                 .Build().Run();
+            //.UseUrls(hostUrl)
 
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, IConfiguration configuration)
+        {
+
+            string allowedHosts = configuration["AllowedHosts"] ?? "127.0.0.1";
+            if (allowedHosts == "*") allowedHosts = "0.0.0.0";
+            string certificateFile = configuration["certificate:file"] ?? "sess-dev.p12";
+            string certificatePassword = configuration["certificate:password"] ?? "sess-dev";
+
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                    {
+                        options.Listen(IPAddress.Parse(allowedHosts), 5000);
+                        options.Listen(IPAddress.Parse(allowedHosts), 5001, listenOptions =>
+                        {
+                            listenOptions.UseHttps(certificateFile, certificatePassword);
+                        } );
+                    }
+                )
                 .UseStartup<Startup>();
+        }
     }
 }
